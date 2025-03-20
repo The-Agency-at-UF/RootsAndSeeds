767118d55cc18cafa731179c610cc50c7878beea
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2025-03-20 11:53:23 -0400

    feat: created react and tailwind template

---
+++ ras-frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+     <p className="text-black-500 text-2xl underline font-bold"> Begin Coding Here! </p>
+    </div>
+  );
+}
+
+export default App;
